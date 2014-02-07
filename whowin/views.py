bc968eadab7616b370b9c1935ad934be7d37f563
James Campbell <james.campbell@tanti.org.uk>
2014-02-07 17:03:52 +0000

    create Match view

--- whowin/views.py
+++ whowin/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
 
-
 from whowin.models import Fight
+
+class MatchView(generic.MatchView):
+	model = Fight
+	template_name = 'fight/match.html'
