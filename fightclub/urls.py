4b05c639871299d225e321eceb114ca0738666c1
James Campbell <james.campbell@tanti.org.uk>
2014-02-08 16:50:42 +0000

    removes final templates and updates urls

--- fightclub/urls.py
+++ fightclub/urls.py
@@ -7,7 +7,6 @@ from django.contrib import admin
 
 
 urlpatterns = patterns("",
-    #url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
     url(r"^admin/", include(admin.site.urls)),
     url(r"^account/", include("account.urls")),
     url(r'^$', include('whowin.urls')),
