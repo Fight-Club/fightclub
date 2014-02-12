fde94c0222e83f21ff4937d0dfee2e33831037df
James Campbell <james.campbell@tanti.org.uk>
2014-02-12 15:18:23 +0000

    sorts out urls

--- fightclub/urls.py
+++ fightclub/urls.py
@@ -1,15 +1,18 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from django.conf.urls import patterns, include, url
+from fights.views import FightView, random_fight
 
 from django.contrib import admin
+admin.autodiscover()
 
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^account/', include("account.urls")),
-    url(r'^whowin/', include('whowin.urls')),
+    url(r'^$', random_fight, name='home'),
+    url(r'^fight/(?P<fight_id>\d+)/$', FightView.as_view(), name='fight_view'),
 )
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
