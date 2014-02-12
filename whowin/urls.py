9e6dc8ee4f9b007bcefa81b60ba271a785d4affe
James Campbell <james.campbell@tanti.org.uk>
2014-02-12 15:48:06 +0000

    fixes urls

--- whowin/urls.py
+++ whowin/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import patterns, url
-from whowin.views import FightView, RankResultsView
+from whowin.views import FightView
 
 urlpatterns = patterns('',
     url(r'^$', FightView.as_view(), name='home'),
-    url(r'^match/(?P<fight_id>\d+)/$', FightView.as_view(), name='fight_view'),
-    url(r'^results/$', RankResultsView.as_view()),
 )
