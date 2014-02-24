94f2737c4621a10400e6d2c7228ec58c88ed4f3c
James Campbell <james.campbell@tanti.org.uk>
2014-02-24 16:02:28 +0000

    almost fixes urls

--- fightclub/urls.py
+++ fightclub/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from whowin.views import RankResultsView
 admin.autodiscover()
 
 
--- whowin/urls.py
+++ whowin/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
-from whowin.views import FightView, FighterListView, RankResultsView
+from whowin.views import FightView, FighterListView, RankResultsView, FighterDetailView
 
 urlpatterns = patterns('',
     url(r'^$', FightView.as_view(), name='home'),
-    url(r'fighters/', FighterListView.as_view()),
+    url(r'fighters/$', FighterListView.as_view(), name='list'),
     url(r'results/', RankResultsView.as_view(), name='results'),
+    url(r'fighters/(?P<>\d+)/', FighterDetailView.as_view(), name='detail'),
     
 )
