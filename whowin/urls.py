42de1cd2f6306039689d05f580c1bbbbf88db82d
James Campbell <james.campbell@tanti.org.uk>
2014-02-24 21:06:05 +0000

    changes urls

--- whowin/urls.py
+++ whowin/urls.py
@@ -3,8 +3,8 @@ from whowin.views import FightView, FighterListView, RankResultsView, FighterDet
 
 urlpatterns = patterns('',
     url(r'^$', FightView.as_view(), name='fight'),
-    url(r'fighters/$', FighterListView.as_view(), name='list'),
-    url(r'results/$', RankResultsView.as_view(), name='results'),
-    url(r'fighters/(?P<slug>[\w-]+)/', FighterDetailView.as_view(), name='detail'),
+    url(r'^fighters/$', FighterListView.as_view(), name='list'),
+    url(r'^results/$', RankResultsView.as_view(), name='results'),
+    url(r'^fighters/(?P<slug>[\w-]+)/', FighterDetailView.as_view(), name='detail'),
     
 )
