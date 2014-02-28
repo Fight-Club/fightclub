14955ebafd57d102f666d4a8a98e65c6c8b2b7db
James Campbell <james.campbell@tanti.org.uk>
2014-02-28 15:33:30 +0000

    changes title

--- whowin/forms.py
+++ whowin/forms.py
@@ -5,6 +5,6 @@ class FighterSelectForm(forms.Form):
     def __init__(self, *args, **kwargs):
         choices = kwargs.pop("choices", [])
         super(FighterSelectForm, self).__init__(*args, **kwargs)
-        self.fields["fighter_choices"] = forms.ChoiceField(
+        self.fields["who_would_win_in_a_fight_between"] = forms.ChoiceField(
             widget=forms.RadioSelect,
             choices=choices)
