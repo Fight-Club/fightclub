4f509556fb012cf890fe949fe6e592415f70effb
James Campbell <james.campbell@tanti.org.uk>
2014-03-24 15:35:02 +0000

    creates a message sent page

--- whowin/views.py
+++ whowin/views.py
@@ -122,6 +122,9 @@ class ContactView(FormView):
         return super(ContactView, self).form_valid(form)
 
 
+class SuccessView(TemplateView):
+    template_name = 'whowin/contactsuccess.html'
+
 
 def home_view(request):
     last = Fighter.objects.count() - 1
