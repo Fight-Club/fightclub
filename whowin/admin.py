943a111bc33971fdcefb1bd618b3aa0364e8afc3
James Campbell <james.campbell@tanti.org.uk>
2014-11-27 15:38:05 +0000

    displays time on list

--- whowin/admin.py
+++ whowin/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from whowin.models import Fighter, Fight
 
 class FightAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'end')
     readonly_fields = ('end',)
 
 admin.site.register(Fighter)
