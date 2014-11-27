e38d32d7801a8ae8f682fc498f4ec9cbbb1d0d31
James Campbell <james.campbell@tanti.org.uk>
2014-11-27 16:07:50 +0000

    moves action bar in admin

--- whowin/admin.py
+++ whowin/admin.py
@@ -5,11 +5,15 @@ from whowin.models import Fighter, Fight
 class FightAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'end', 'user')
     readonly_fields = ('end',)
+    actions_on_bottom = True
+    actions_on_top = False
 
 
 class FighterAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'rating')
     ordering = ('-rating',)
+    actions_on_bottom = True
+    actions_on_top = False
 
 
 admin.site.register(Fighter, FighterAdmin)
