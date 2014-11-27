b63763b70c8ca3c2d5b90cbe66fa37815a9d4f05
James Campbell <james.campbell@tanti.org.uk>
2014-11-27 13:58:08 +0000

    shows date event occurred

--- whowin/admin.py
+++ whowin/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from whowin.models import Fighter, Fight
 
+class FightAdmin(admin.ModelAdmin):
+    readonly_fields = ('end',)
+
 admin.site.register(Fighter)
-admin.site.register(Fight)
+admin.site.register(Fight, FightAdmin)
