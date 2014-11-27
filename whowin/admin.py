c67f9f6c6c153fffa941640ab3170af6b64ee1ca
James Campbell <james.campbell@tanti.org.uk>
2014-11-27 15:40:49 +0000

    add user to list display

--- whowin/admin.py
+++ whowin/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from whowin.models import Fighter, Fight
 
+
 class FightAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__', 'end')
+    list_display = ('__unicode__', 'end', 'user')
     readonly_fields = ('end',)
 
 admin.site.register(Fighter)
